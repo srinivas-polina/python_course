c63817a195d180bfce25c46a083a9e57a39a8e42
srinivaspolina <srinivaspolina7@gmail.com>
2026-03-03 15:57:50 -0600

    Change

---
+++ InheritanceDemo.py
@@ -0,0 +1,3 @@
+#we are going to use inheritance to add different types of employee classes to our employee.py file.
+#our company now not only has employees that ear a salary, but also hourly and commision employees.
+#so we want to create a general employee class for three types of employees to inherit it from salary employee, Hourly Employee, and Commision Employee.
